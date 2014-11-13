22d511fb5c274173cf3b556f9b78a873f9793729
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-11-13 10:36:58 +0100

    feito

---
+++ src/boletin9/pkg2/Boletin92.java
@@ -0,0 +1,11 @@
+
+package boletin9.pkg2;
+
+public class Boletin92 {
+
+    public static void main(String[] args) {
+        Sumprod obx=new Sumprod();
+        obx.calcular();
+    }
+    
+}
---
+++ src/boletin9/pkg2/Sumprod.java
@@ -0,0 +1,24 @@
+
+package boletin9.pkg2;
+
+import javax.swing.JOptionPane;
+
+public class Sumprod {
+    public Sumprod(){
+        }
+ public void calcular() {
+int acS=0;
+double acP=1;
+for (int i = 10; i < 90; i++) {
+acS = i + acS;
+acP = i * acP;
+}
+JOptionPane.showMessageDialog(null,"Suma= "+acS);
+JOptionPane.showMessageDialog(null,"Producto= "+acP);
+}
+}
+
+
+    
+
+
