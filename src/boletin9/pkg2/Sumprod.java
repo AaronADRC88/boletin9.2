8808b9e1da9e1fdfe1da058ac8b0e9d95e911913
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-11-14 12:20:40 +0100

    feito e mellorado

--- src/boletin9/pkg2/Sumprod.java
+++ src/boletin9/pkg2/Sumprod.java
@@ -13,8 +13,7 @@ for (int i = 10; i < 90; i++) {
 acS = i + acS;
 acP = i * acP;
 }
-JOptionPane.showMessageDialog(null,"Suma= "+acS);
-JOptionPane.showMessageDialog(null,"Producto= "+acP);
+JOptionPane.showMessageDialog(null,"Suma= "+acS+"Producto= "+acP);
 }
 }
 
